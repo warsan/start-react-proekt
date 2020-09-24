2a26535b71ad215c31887b04fd5ce6ff49398114
Сандро <warsan00@mail.ru>
2020-09-24 07:11:40 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Привет, CodeSandbox</h1>
+      <h2>Начните редактировать, чтобы увидеть волшебство!</h2>
+    </div>
+  );
+}
